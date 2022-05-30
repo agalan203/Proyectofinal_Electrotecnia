485e14b53d2de87a03c93ca07a4fee4ef4bdabb3
Albertina Galan <agalan@itba.edu.ar>
2022-05-30 19:29:57 -0300

    30/5 7:29 GUI completa salvo funcion transferencia

--- mainwindow.py
+++ mainwindow.py
@@ -1,12 +1,14 @@
-from GUI.GUIv4 import *
+from GUI.GUIv5 import *
 import numpy as np
 import sys
+import io
 import os
 from PyQt5.QtWidgets import QMainWindow, QFileDialog, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox, QRadioButton, QInputDialog
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 
 import scipy.signal as signal
+import control
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -18,7 +20,11 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
+        self.initGraphs()
+        self.updatePlots()
+        self.updateTransferFunctions()
 
+    def initGraphs(self):
         #GRAFICOS
         # Grafico Amplitud Filtros 1er orden
         self.figure1_amp = Figure()
@@ -26,7 +32,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.axes1_amp = self.figure1_amp.subplots()
         self.bodeamplitudelayout_1.addWidget(NavigationToolbar(self.canvas1_amp, self))
         self.bodeamplitudelayout_1.addWidget(self.canvas1_amp)
-
         self.axes1_amp.set_xscale('log')
 
         # Grafico Fase Filtros 1er orden
@@ -35,7 +40,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.axes1_fase = self.figure1_fase.subplots()
         self.bodefaselayout_1.addWidget(NavigationToolbar(self.canvas1_fase, self))
         self.bodefaselayout_1.addWidget(self.canvas1_fase)
-
         self.axes1_fase.set_xscale('log')
 
         # Grafico Ceros Filtros 1er orden
@@ -58,7 +62,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.axes2_amp = self.figure2_amp.subplots()
         self.bodeamplitudelayout_2.addWidget(NavigationToolbar(self.canvas2_amp, self))
         self.bodeamplitudelayout_2.addWidget(self.canvas2_amp)
-
         self.axes2_amp.set_xscale('log')
 
         # Grafico Fase Filtros 2do orden
@@ -67,7 +70,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.axes2_fase = self.figure2_fase.subplots()
         self.bodefaselayout_2.addWidget(NavigationToolbar(self.canvas2_fase, self))
         self.bodefaselayout_2.addWidget(self.canvas2_fase)
-
         self.axes2_fase.set_xscale('log')
 
         # Grafico Ceros Filtros 2do orden
@@ -90,7 +92,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.axesrlc_amp = self.figurerlc_amp.subplots()
         self.bodeamplitudelayout_rlc.addWidget(NavigationToolbar(self.canvasrlc_amp, self))
         self.bodeamplitudelayout_rlc.addWidget(self.canvasrlc_amp)
-
         self.axesrlc_amp.set_xscale('log')
 
         # Grafico Fase RLC
@@ -99,7 +100,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.axesrlc_fase = self.figurerlc_fase.subplots()
         self.bodefaselayout_rlc.addWidget(NavigationToolbar(self.canvasrlc_fase, self))
         self.bodefaselayout_rlc.addWidget(self.canvasrlc_fase)
-
         self.axesrlc_fase.set_xscale('log')
 
         # Grafico Salida RLC
@@ -108,6 +108,96 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.axesrlc_out = self.figurerlc_out.subplots()
         self.outputlayout_rlc.addWidget(NavigationToolbar(self.canvasrlc_out, self))
         self.outputlayout_rlc.addWidget(self.canvasrlc_out)
+
+    def updatePlots(self):
+        # Filtros de 1er orden
+        self.axes1_amp.set_xlabel('$Frecuencia [rad/s]$')
+        self.axes1_amp.set_ylabel('$|H(jw)| [dB]$')
+        self.axes1_amp.grid(True, which='both')
+        self.figure1_amp.set_tight_layout('True')
+        self.figure1_amp.tight_layout()
+        self.canvas1_amp.draw()
+
+        self.axes1_fase.set_xlabel('$Frecuencia [rad/s]$')
+        self.axes1_fase.set_ylabel('$Fase [\u00B0]$')
+        self.axes1_fase.grid(True, which='both')
+        self.figure1_fase.set_tight_layout('True')
+        self.figure1_fase.tight_layout()
+        self.canvas1_fase.draw()
+
+        self.axes1_ceros.set_xlabel('$\u03C3$')
+        self.axes1_ceros.set_ylabel('$j\omega$')
+        self.axes1_ceros.grid(True, which='both')
+        self.figure1_ceros.tight_layout()
+        self.figure1_ceros.set_tight_layout('True')
+        self.canvas1_ceros.draw()
+
+        self.axes1_out.set_xlabel('$t [s]$')
+        self.axes1_out.set_ylabel('$f(t)$')
+        self.axes1_out.grid(True, which='both')
+        self.figure1_out.set_tight_layout('True')
+        self.figure1_out.tight_layout()
+        self.canvas1_out.draw()
+
+        # Filtros de 2do orden
+        self.axes2_amp.set_xlabel('$Frecuencia [rad/s]$')
+        self.axes2_amp.set_ylabel('$|H(jw)| [dB]$')
+        self.axes2_amp.grid(True, which='both')
+        self.figure2_amp.set_tight_layout('True')
+        self.figure2_amp.tight_layout()
+        self.canvas2_amp.draw()
+
+        self.axes2_fase.set_xlabel('$Frecuencia [rad/s]$')
+        self.axes2_fase.set_ylabel('$Fase [\u00B0]$')
+        self.axes2_fase.grid(True, which='both')
+        self.figure2_fase.set_tight_layout('True')
+        self.figure2_fase.tight_layout()
+        self.canvas2_fase.draw()
+
+        self.axes2_ceros.set_xlabel('$\u03C3$')
+        self.axes2_ceros.set_ylabel('$j\omega$')
+        self.axes2_ceros.grid(True, which='both')
+        self.figure2_ceros.tight_layout()
+        self.figure2_ceros.set_tight_layout('True')
+        self.canvas2_ceros.draw()
+
+        self.axes2_out.set_xlabel('$t [s]$')
+        self.axes2_out.set_ylabel('$f(t)$')
+        self.axes2_out.grid(True, which='both')
+        self.figure2_out.set_tight_layout('True')
+        self.figure2_out.tight_layout()
+        self.canvas2_out.draw()
+
+        # RLC
+        self.axesrlc_amp.set_xlabel('$Frecuencia [rad/s]$')
+        self.axesrlc_amp.set_ylabel('$|H(jw)| [dB]$')
+        self.axesrlc_amp.grid(True, which='both')
+        self.figurerlc_amp.set_tight_layout('True')
+        self.figurerlc_amp.tight_layout()
+        self.canvasrlc_amp.draw()
+
+        self.axesrlc_fase.set_xlabel('$Frecuencia [rad/s]$')
+        self.axesrlc_fase.set_ylabel('$Fase [\u00B0]$')
+        self.axesrlc_fase.grid(True, which='both')
+        self.figurerlc_fase.set_tight_layout('True')
+        self.figurerlc_fase.tight_layout()
+        self.canvasrlc_fase.draw()
+
+        self.axesrlc_out.set_xlabel('$t [s]$')
+        self.axesrlc_out.set_ylabel('$f(t) [V]$')
+        self.axesrlc_out.grid(True, which='both')
+        self.figurerlc_out.set_tight_layout('True')
+        self.figurerlc_out.tight_layout()
+        self.canvasrlc_out.draw()
+
+
+    def updateTransferFunctions(self):
+        #TODO esto no supe como resolverlo
+        self.pixmapfunciontransferencia_1.setText("H(s) =" )
+        self.pixmapfunciontransferencia_2.setText("H(s) =")
+        self.pixmapfunciontransferencia_rlc.setText("H(s) =")
+
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
